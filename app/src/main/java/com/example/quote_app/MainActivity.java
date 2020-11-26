70662f37d2284bb4e33b8faa52ccc30dc3001c70
Ptr <petarman96@abv.bg>
2020-11-26 13:21:46 +0200

    Designed a toolbar and added it to the app, tabs are to be added

--- app/src/main/java/com/example/quote_app/MainActivity.java
+++ app/src/main/java/com/example/quote_app/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.quote_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setToolBar();
+
+
+    }
+
+
+    private void setToolBar(){
+        toolBar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
     }
 }
