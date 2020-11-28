3d7061d504df730842fe1889009bf5a637676e9e
Ptr <petarman96@abv.bg>
2020-11-28 19:43:54 +0200

    Finished singleton Database class

--- app/src/main/java/com/example/quote_app/database/Database.java
+++ app/src/main/java/com/example/quote_app/database/Database.java
@@ -1,5 +1,24 @@
 package com.example.quote_app.database;
 
+import android.content.Context;
+
+import androidx.room.Room;
+
 public final class Database {
+
+    private static final String DATABASE_NAME = "com.example.dailysmarts";
+
+    private static AppDatabase instance;
+
+    private Database() {}
+
+    public static AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context, AppDatabase.class, DATABASE_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 
 }
