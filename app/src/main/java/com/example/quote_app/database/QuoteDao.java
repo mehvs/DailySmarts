f20373f29d8ad23924acbb60e9606f5564189f39
Ptr <petarman96@abv.bg>
2020-11-28 20:53:59 +0200

    converted the class QuoteDao to interface and added some basic stuff

--- app/src/main/java/com/example/quote_app/database/QuoteDao.java
+++ app/src/main/java/com/example/quote_app/database/QuoteDao.java
@@ -1,5 +1,22 @@
 package com.example.quote_app.database;
 
-public class QuoteDao {
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface QuoteDao {
+
+    @Query("SELECT * FROM quotes")
+    List<Quote> getAll();
+
+    @Insert
+    void insertAll(Quote... quotes);
+
+    @Delete
+    void delete(Quote quote);
 
 }
