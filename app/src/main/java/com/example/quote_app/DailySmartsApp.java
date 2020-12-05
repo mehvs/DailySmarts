b01636ab2d51e3111c38a9f5c3331fc08c5b136f
Цветослав Тодоров <gavril3vr@gmail.com>
2020-12-05 11:57:05 +0200

    startClass

---
+++ app/src/main/java/com/example/quote_app/DailySmartsApp.java
@@ -0,0 +1,12 @@
+package com.example.quote_app;
+
+import android.app.Application;
+
+public class DailySmartsApp extends Application {
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+    }
+}
